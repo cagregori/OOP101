9dde9b8d53380e983dbc78c4ec0c5cf304c5473d
cagregori <cagregori@fit.edu.ph>
2026-09-07 12:37:26 +0800

    Refactor program to calculate gross salary
    Updated the program to calculate gross salary based on user input for hours worked and hourly rate.

---
+++ Mod 2-3/Exercise 1/Gregori-Mod3-Problem1.java
@@ -0,0 +1,21 @@
+//Gregori, Christian Jowell A. TA21
+
+import java.util.Scanner;
+
+public class Mod3Problem1 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      
+      int hrsWorked; int hrlyRate;
+
+      //Get hours worked integer
+      System.out.print("Enter hours worked: ");
+      hrsWorked = scanner.nextInt();
+
+      //Get hourly rate integer
+      System.out.print("Enter hours worked: ");
+      hrlyRate = scanner.nextInt();
+
+      System.out.println("Gross Salary = " + (hrsWorked*hrlyRate));
+    }
+}
