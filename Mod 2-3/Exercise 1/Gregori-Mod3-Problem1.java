c7c8fe7130899f2bb56a82b029b97341b17a3678
cagregori <cagregori@fit.edu.ph>
2026-09-09 15:53:09 +0800

    Update Gregori-Mod3-Problem1.java

--- Mod 2-3/Exercise 1/Gregori-Mod3-Problem1.java
+++ Mod 2-3/Exercise 1/Gregori-Mod3-Problem1.java
@@ -1,22 +1,22 @@
-//Gregori, Christian Jowell A. TA21
+//Gregori, Christian Jowell A.
 
 import java.util.Scanner;
 
-public class Mod3Problem1 {
+public class Mod3Prob1 {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      
-      double hrsWorked; int hrlyRate;
+        Scanner scanner = new Scanner(System.in);
+
+        double hrsWorked; int hrlyRate;
 
         //Get hours worked integer
-      System.out.print("Enter hours worked: ");
-      hrsWorked = scanner.nextDouble();
+        System.out.print("Enter hours worked: ");
+        hrsWorked = scanner.nextDouble();
 
-      //Get hourly rate integer
-      System.out.print("Enter hours worked: ");
-      hrlyRate = scanner.nextInt();
+        //Get hourly rate integer
+        System.out.print("Enter hourly rate: ");
+        hrlyRate = scanner.nextInt();
 
-      System.out.println();
-      System.out.println("Gross Salary = " + (hrsWorked*hrlyRate));
+        System.out.println();
+        System.out.println("Gross Salary = " + (hrsWorked*hrlyRate));
     }
 }
